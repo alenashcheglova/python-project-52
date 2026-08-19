898009c1e85dcd713d48a68e6184a8aae18af201
Alena Shcheglova <alenn521@icloud.com>
2026-08-19 11:26:35 +0300

    update manage.py

--- manage.py
+++ manage.py
@@ -7,6 +7,8 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+    if "tailwind" in sys.argv:
+        return
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
