8bbcc6260f65cabeec7f8c5e6d1341554cb9aebe
Alena Shcheglova <alenn521@icloud.com>
2026-08-20 14:53:28 +0300

    fix coverage

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # скачиваем uv
-curl -LsSf https://astral.sh/uv/install.sh | sh
+curl --proto "=https" --tlsv1.2 -LsSf https://astral.sh/uv/install.sh | sh
 source $HOME/.local/bin/env
 
 
